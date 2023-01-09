ad04d61305263d15225c6a11b860e7eb67cefaf5
thebabellibrarybot <jack@thebabellibrary.com>
2023-01-09 16:37:47 -0500

    v2

--- pack_venv/app.py
+++ pack_venv/app.py
@@ -1,5 +1,6 @@
 from src import pack
 from src import space
+from src.pack import pack_tight
 
 xml_fi = 'DATA/jan_pack.xlsx'
 location = {}
@@ -8,6 +9,7 @@ location['location 1'] = ( 3, [(96,120,60),(96,96,60),(48,48,60)], [['Custom Cra
 def cleanup(i):
     wrong = i.in_wrong_location()
     didnt_fit = i.cant_fit()
+    # TODO map and list wrong & didnt_fit
     return wrong, didnt_fit
 
 def get_ls(xml_fi):
@@ -21,11 +23,20 @@ def get_ls(xml_fi):
     possible_dics = i.pack_types_in_spaces()
     master_list = i.in_right_location()
 
+    # TODO pack master_list according to possible_dics
+    # where each packed_item is removed from master_list
+    # and sorted into storage_spaces
+
+    packed_tight = pack_tight(master_list, possible_dics, location)
+    print(packed_tight)
+
+
+
     # grab items in the wrong space
     # grab items that didn't fit
     if len(master_list) != len(dic):
         dirt = cleanup(i)
-        print(len(dirt[0]), len(dirt[1]))
+        #print(len(dirt[0]), len(dirt[1]))
 
 
 
--- pack_venv/src/pack.py
+++ pack_venv/src/pack.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
+# creates a dict of items found in the input xml_fi
+# needs a xml_fi formated so that
+# 'Unnamed:0': 'view',
+#    'Unnamed: 1': 'Pack_ID',
+#    'Unnamed: 2': 'HM',
+#   'Unnamed: 3': 'ARTnum',
+#   'Unnamed: 4': 'client',
+#    'Unnamed: 5': 'artist',
+#    'Unnamed: 6': 'dims',
+#   'Unnamed: 7': 'location',
+#    'Unnamed: 12': 'packed'
+
 class item_ls():
 
     def __init__(self, xml_fi):
@@ -11,7 +23,15 @@ class item_ls():
         hm_dic = {}
 
         df = pd.read_excel(self.xml_fi)
-        workbook = df.rename(columns={'Unnamed:0': 'view', 'Unnamed: 1': 'Pack_ID', 'Unnamed: 2': 'HM', 'Unnamed: 3': 'ARTnum', 'Unnamed: 4': 'client', 'Unnamed: 5': 'artist', 'Unnamed: 6': 'dims', 'Unnamed: 7': 'location', 'Unnamed: 12': 'packed'})
+        workbook = df.rename(columns={'Unnamed:0': 'view',
+                                        'Unnamed: 1': 'Pack_ID',
+                                         'Unnamed: 2': 'HM',
+                                          'Unnamed: 3': 'ARTnum',
+                                           'Unnamed: 4': 'client',
+                                            'Unnamed: 5': 'artist',
+                                             'Unnamed: 6': 'dims',
+                                              'Unnamed: 7': 'location',
+                                               'Unnamed: 12': 'packed'})
 
         book_len = workbook.shape[0]
 
@@ -30,3 +50,30 @@ class item_ls():
             hm_dic[workbook.HM[num]] = info
 
         return hm_dic
+
+# args: master_list, possible_dics
+
+# where master_list == a dictionary of all items that are in the correct 
+# storage facility. this list will be chipped away at until it is empty or 
+# the space is full
+# from: src/space.py
+
+# where possible_dics == a dictionary of all storage_spaces in the storage_facility
+# along with all the dictionary items from the master_list which fit into that
+# particular storage_space. master_list items being packed must also be in the 
+# possible_dics list in order to be allowed into a storage_space
+
+def pack_tight(master_list, possible_dics, location):
+
+    loc = list(location['location 1'])
+    print(loc[0])
+
+    return 'hello world'
+
+# allow stacking?
+
+# room for err?
+
+# append all info?
+
+# cavalier == guest password
